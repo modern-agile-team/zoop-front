3bd1b040c6bc03707666e5a13ca1a2b1ddefb28c
soonki-98 <alstnsrl98@gmail.com>
2025-06-06 17:19:56 +0900

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,58 @@
+import { type Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#E0F2FE',
+          200: '#BAE6FD',
+          300: '#7DD3FC',
+          400: '#38BDF8',
+          500: '#0EA5E9',
+          600: '#0284C7',
+          700: '#0369A1',
+          800: '#075985',
+          900: '#0C4A6E',
+        },
+        secondary: {
+          100: '#F3E8FF',
+          200: '#E9D5FF',
+          300: '#D8B4FE',
+          400: '#C084FC',
+          500: '#A855F7',
+          600: '#9333EA',
+          700: '#7E22CE',
+          800: '#6B21A8',
+          900: '#581C87',
+        },
+        contents: {
+          100: '#F3F4F6',
+          200: '#E5E7EB',
+          300: '#D1D5DB',
+          400: '#9CA3AF',
+          500: '#6B7280',
+          600: '#4B5563',
+          700: '#374151',
+          800: '#1F2937',
+          900: '#111827',
+        },
+        background: {
+          100: '#FFFDF9',
+          200: '#FDF9F3',
+          300: '#F8F6F2',
+          400: '#ECEAE5',
+          500: '#DCD9D1',
+          600: '#BAB7B0',
+          700: '#9B9891',
+          800: '#6F6D68',
+          900: '#44423E',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
