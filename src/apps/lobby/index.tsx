f446376b741044cf00e646a26336f7b3e2b44445
soonki-98 <alstnsrl98@gmail.com>
2025-06-29 16:46:29 +0900

    로비비 화면 UI 개선

--- src/apps/lobby/index.tsx
+++ src/apps/lobby/index.tsx
@@ -1,28 +1,49 @@
-import { ScrollArea } from '@/components/ui/scroll-area';
+import { Button } from '@/components/ui/button';
 import Room from './components/room';
+import { LobbyScrollSection } from './components/Section';
 
-const MOCK_ROOMS = Array.from({ length: 30 }, (_, i) => i + 1);
+const MOCK_ROOMS = Array.from({ length: 20 }, (_, i) => i + 1);
 
 export default function LobbyPage() {
   return (
-    <div className="flex flex-col h-screen bg-primary-900">
-      <header className="w-full">
-        <h1
-          style={{ textShadow: '1px 1px 4px rgba(0,0,0,0.6)' }}
-          className="text-title-1 text-white"
-        >
-          로비
-        </h1>
-      </header>
-      <section className="flex flex-1 min-h-0">
-        <ScrollArea className="h-full">
-          <ul className="flex flex-col gap-4 p-4">
-            {MOCK_ROOMS.map((room) => (
-              <Room key={room} />
-            ))}
-          </ul>
-        </ScrollArea>
-      </section>
+    <div className="flex flex-col h-screen bg-bg-800">
+      <div className="grid grid-cols-4 h-full gap-8 py-16 flex-1 justify-center">
+        <LobbyScrollSection>
+          <LobbyScrollSection.Header>참여자 리스트</LobbyScrollSection.Header>
+          <LobbyScrollSection.Content></LobbyScrollSection.Content>
+        </LobbyScrollSection>
+
+        <LobbyScrollSection>
+          <LobbyScrollSection.Header>대기방 목록</LobbyScrollSection.Header>
+          <LobbyScrollSection.Content>
+            <ul className="flex flex-col gap-4 p-4 w-full">
+              {MOCK_ROOMS.map((room) => (
+                <Room key={room} />
+              ))}
+            </ul>
+          </LobbyScrollSection.Content>
+          <LobbyScrollSection.Footer>
+            <Button className="flex-1">방 만들기</Button>
+          </LobbyScrollSection.Footer>
+        </LobbyScrollSection>
+
+        <LobbyScrollSection>
+          <LobbyScrollSection.Header>
+            진행중인 방 목록
+          </LobbyScrollSection.Header>
+          <LobbyScrollSection.Content>
+            <ul className="flex flex-col gap-4 p-4">
+              {MOCK_ROOMS.map((room) => (
+                <Room key={room} />
+              ))}
+            </ul>
+          </LobbyScrollSection.Content>
+        </LobbyScrollSection>
+
+        <LobbyScrollSection>
+          <LobbyScrollSection.Header>공지사항</LobbyScrollSection.Header>
+        </LobbyScrollSection>
+      </div>
     </div>
   );
 }
