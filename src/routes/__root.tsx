57424d7ed285094060e6df1d2caa3da60fff8887
soonki-98 <alstnsrl98@gmail.com>
2025-08-18 13:29:38 +0900

    feat: 소켓 연결 #32

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,11 +2,24 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet, createRootRoute, useLocation } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { OverlayProvider } from 'overlay-kit';
+import { useEffect } from 'react';
+
+import { socketClient } from '@/shared/service/socket/client';
 
 const queryClient = new QueryClient();
 
 const Component = () => {
   const location = useLocation();
+  useEffect(() => {
+    const socket = socketClient.getSocket();
+
+    socketClient.connect();
+    return () => {
+      if (socket?.connected) {
+        socket.close();
+      }
+    };
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
