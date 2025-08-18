0b48f455699a7b7a04e3f246a41459938e90688c
soonki-98 <alstnsrl98@gmail.com>
2025-08-18 21:41:59 +0900

    feat: 소켓 이벤트를 s2c와 c2s 구분 #41

--- src/lib/asyncApi/_generated/types.ts
+++ src/lib/asyncApi/_generated/types.ts
@@ -5,45 +5,31 @@ export type { AccountEnteredSocketEventBody } from './models/AccountEnteredSocke
 export type { AccountEnteredSocketEvent } from './models/AccountEnteredSocketEvent';
 
 import type { AccountEnteredSocketEvent } from './models/AccountEnteredSocketEvent';
-import type { AccountEnteredSocketEventBody } from './models/AccountEnteredSocketEventBody';
-import type { AccountEnteredSocketEventBodyAccount } from './models/AccountEnteredSocketEventBodyAccount';
 
-export const enum SocketEventNames {
+export const enum ServerToClientEventNames {
   ACCOUNT_ENTERED = 'account.entered',
 }
 
-// 유틸리티 타입들
-export type SocketEventData<T extends SocketEventNames> = Parameters<
-  ServerToClientEvents[T]
->[0];
-
-// Socket.io 이벤트 맵 정의
-export interface ServerToClientEvents {
-  /** 유저가 접속했을 때 발생하는 이벤트 */
-  [SocketEventNames.ACCOUNT_ENTERED]: (data: AccountEnteredSocketEvent) => void;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface ClientToServerEvents {
+export const enum ClientToServerEventNames {
   // 현재 클라이언트에서 서버로 보내는 이벤트가 정의되지 않았습니다
   // AsyncAPI 스펙에서 subscribe 이벤트를 추가하면 여기에 자동으로 생성됩니다
 }
 
-// 타입 가드 함수들
-export const isAccountEnteredSocketEventBodyAccount = (
-  data: unknown
-): data is AccountEnteredSocketEventBodyAccount => {
-  return typeof data === 'object' && data !== null && 'eventName' in data;
-};
+// 유틸리티 타입들
+export type ServerToClientEventData<T extends ServerToClientEventNames> =
+  Parameters<ServerToClientEvents[T]>[0];
 
-export const isAccountEnteredSocketEventBody = (
-  data: unknown
-): data is AccountEnteredSocketEventBody => {
-  return typeof data === 'object' && data !== null && 'eventName' in data;
-};
+export type ClientToServerEventData<T extends ClientToServerEventNames> =
+  ClientToServerEvents[T];
 
-export const isAccountEnteredSocketEvent = (
-  data: unknown
-): data is AccountEnteredSocketEvent => {
-  return typeof data === 'object' && data !== null && 'eventName' in data;
-};
+// Socket.io 이벤트 맵 정의
+export interface ServerToClientEvents {
+  /** 유저가 접속했을 때 발생하는 이벤트 */
+  [ServerToClientEventNames.ACCOUNT_ENTERED]: (
+    data: AccountEnteredSocketEvent
+  ) => void;
+}
+
+export interface ClientToServerEvents {
+  [eventName: string]: unknown;
+}
