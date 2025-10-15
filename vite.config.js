20e7ee61f6ee623640afdb9e3253e13baf1d25f0
soonki-98 <alstnsrl98@gmail.com>
2025-10-15 14:50:29 +0900

    chore: port 3000으로 고정 #105

--- vite.config.js
+++ vite.config.js
@@ -18,9 +18,8 @@ const ReactCompilerConfig = {
 };
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const appPort = Number(env.VITE_APP_PORT) || 3000;
+export default defineConfig(() => {
+  const appPort = 3000;
   return {
     plugins: [
       TanStackRouterVite({ autoCodeSplitting: true }),
