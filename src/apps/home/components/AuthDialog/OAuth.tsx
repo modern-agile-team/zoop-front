f8a978a9737080b66930c92dd89321c0c660221d
zzzryt <jinjinstar3@gmail.com>
2025-10-27 14:28:23 +0900

    fix: icon 경로 불러오기 변경 #87

--- src/apps/home/components/AuthDialog/OAuth.tsx
+++ src/apps/home/components/AuthDialog/OAuth.tsx
@@ -1,3 +1,5 @@
+import GoogleIcon from '/google_icon.svg';
+
 export default function OAuth() {
   const handleGoogleLogin = () => {
     window.open(`${import.meta.env.VITE_API_URL}/auth/google`, '_self');
@@ -7,7 +9,7 @@ export default function OAuth() {
       onClick={handleGoogleLogin}
       className="bg-[#F2F2F2] text-[#1F1F1F] font-(--font-roboto) py-[10px] px-[12px] gap-[10px] flex justify-center items-center rounded-2xl"
     >
-      <img src={'/google_icon.svg'} alt="구글 로그인" width={20} height={20} />
+      <img src={GoogleIcon} alt="구글 로그인" width={20} height={20} />
       <span>Google로 로그인</span>
     </button>
   );
